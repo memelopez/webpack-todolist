0deae6582ba864478068a31921c1db7495f9a836
memelopez <meme30lopez@gmail.com>
2021-08-18 16:02:38 -0600

    Change entre and title

--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,14 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  // entry: './src/index.js',
-  entry: {
-    index: './src/index.js',
-  },
+  entry: './src/index.js',
   devtool: 'inline-source-map',
   devServer: {
     contentBase: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Development',
+      title: 'Webpack To-do List',
       template: './src/index.html',
     }),
   ],
