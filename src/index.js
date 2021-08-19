f68857783bae06013fc6f5176cf41a6f71774537
memelopez <meme30lopez@gmail.com>
2021-08-18 19:25:20 -0600

    Add events for adding task and for checkboxes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,44 @@
 import './style.css';
 import UI from './ui';
+import validateDescription from './validateDescription';
 
 // When content loads
 document.addEventListener('DOMContentLoaded', UI.addApp());
+
+// Event: when form is submitted
+document.querySelector('#addTaskForm').addEventListener('submit', (e) => {
+  // Prevent actual submit
+  e.preventDefault();
+
+  // Get form values
+  const taskDescription = document.querySelector('#taskDesc').value.trim();
+  if (!validateDescription(taskDescription)) {
+    console.log('Error- empty description');
+  } else {
+    // Add task
+    UI.addTaskStore(taskDescription);
+  }
+
+  // reloads page
+  // eslint-disable-next-line no-restricted-globals
+  location.reload();
+});
+
+document.querySelector('#task-list').addEventListener('change', (e) => {
+  // checks if this is trigerring for the correct element
+  if (e.target.tagName === 'INPUT' && e.target.type === 'checkbox') {
+    // Gets the state of the checked checkbox
+    const checkboxState = e.target.checked;
+
+    const ulList = document.querySelector('#task-list');
+    const itemChecked = e.target.parentElement.parentElement;
+    const nodes = Array.from(ulList.children);
+    const index = nodes.indexOf(itemChecked);
+
+    UI.taskCompleted(index, checkboxState);
+
+    // Reload page
+    // eslint-disable-next-line no-restricted-globals
+    location.reload();
+  }
+});
---
+++ src/store.js
@@ -0,0 +1,43 @@
+// Store Class: Encapsulates de locacl storage
+export default class Store {
+  // gets tasks from storage and returns an array of objects
+  static getTasks() {
+    let tasks;
+    if (localStorage.getItem('tasks') === null) {
+      tasks = [];
+    } else {
+      tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    return tasks;
+  }
+
+  static setTasks(tasks) {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }
+
+  static addTask(task) {
+    const tasks = this.getTasks();
+    tasks.push(task);
+    this.setTasks(tasks);
+  }
+
+  // index - saves the total of tasks created ever
+  static getIndexTotal() {
+    let total;
+    if (localStorage.getItem('index') === null) {
+      total = 0;
+    } else {
+      total = JSON.parse(localStorage.getItem('index'));
+    }
+
+    return total;
+  }
+
+  // increments the index in storage and saves it again
+  static setsIndexTotalPlusOne() {
+    let total = this.getIndexTotal();
+    total += 1;
+    localStorage.setItem('index', JSON.stringify(total));
+  }
+}
