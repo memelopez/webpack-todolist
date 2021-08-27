57acffd55c5b38a11ff606b066104acfafe814e4
DanteAlonsoHT <dante1818@hotmail.com>
2021-08-27 13:11:05 -0500

    Set up mock local storage for testing

--- src/test.js
+++ src/test.js
@@ -2,3 +2,30 @@ import Task from './task';
 import Store from './store';
 import UI from './ui';
 import removeTask from './removeTask';
+
+const eventMock = {
+  preventDefault: () => {},
+  target: {
+    getElementsByTagName: () => [{ value: 'Mock' }],
+  },
+};
+class mockLocalStorage {
+  constructor() {
+    this.storage = {};
+  }
+  getItem(key) {
+    return this.storage[key] || null;
+  }
+  setItem(key, value) {
+    if (value) {
+      this.storage[key] = value;
+    }
+  }
+  removeItem(key) {
+    delete this.storage[key];
+  }
+  clear() {
+    this.storage = {};
+  }
+}
+global.localStorage = new mockLocalStorage();
